f5e438aef5be41abb2f15680112edc2833250eff
Marciel Assiz <marciel.emersom@gmail.com>
2020-03-27 18:13:53 -0300

    Add pasta de dao para mudança  Data Access Object
    D.A.O - Data Access Object

---
+++ src/dao/ClienteDAO.java
@@ -0,0 +1,27 @@
+package dao;
+import java.util.ArrayList;
+
+import models.Cliente;
+
+//MVC
+public class ClienteDAO {
+
+	//DAO - Data Access Object
+	private static ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	
+	public static Boolean cadastrarCliente(Cliente c) {		
+		for (Cliente clienteCadastrado : clientes) {
+			if(clienteCadastrado.getCpf().equals(c.getCpf())) {
+				return false;
+			}
+		}		
+		clientes.add(c);	
+		return true;
+	}
+	
+	public static ArrayList<Cliente> retonarClientes(){
+		return clientes;
+	}
+	
+	
+}
